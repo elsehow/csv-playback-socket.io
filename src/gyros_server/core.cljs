ccc21e40e091883adf2b0f201b5f3506085bc8a2
elsehow <yes@cosmopol.is>
2016-03-08 11:15:20 -0800

    initial

---
+++ src/gyros_server/core.cljs
@@ -0,0 +1,44 @@
+(ns gyros-server.core
+  (:require [cljs.nodejs :as nodejs]))
+
+(nodejs/enable-util-print!)
+
+(def fs
+  (nodejs/require "fs"))
+
+(def io
+  (let [app (.createServer (nodejs/require "http"))]
+    ((nodejs/require "socket.io") app)))
+
+(def csv-file
+   (.readFileSync fs "data.csv"))
+
+(def csv-lines
+  (rest (.split (.toString csv-file) "\n")))
+
+(defn to-map [csv-line]
+  (let [line (.split csv-line ",")]
+    (zipmap [:timestamp :imu :yaw :pitch :roll :rssi] line)))
+
+(def data-points
+  (map to-map csv-lines))
+
+(def time-deltas
+  (let [initial (:timestamp (first data-points))]
+    (map
+      #(- (:timestamp %) initial)
+      data-points)))
+
+(defn set-timeouts [cb]
+  (doall
+    (map
+      (fn [data time] (js/setTimeout #(cb data) time))
+      data-points
+      time-deltas)))
+
+(defn -main []
+  (.on io "connection"
+    (fn [socket]
+      (set-timeouts socket.emit))))
+
+(set! *main-cli-fn* -main)
